465a60e11786a17fc880e5a308f80e26a296ede7
Neeraj <neeraj.s2022@vitstudent.ac.in>
2024-01-25 17:25:54 +0530

    feat: Added basic schema

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className="App">
+     <Counter></Counter>
+    </div>
+  )
+}
+
+export default App
